1382d08ea44a83c087e29ff78bab86e80931feec
Arley <tamirespaulino19@alu.ufc.br>
2023-03-31 11:23:13 -0300

    exercicío concluído

---
+++ main.py
@@ -0,0 +1,24 @@
+numero = int(input('Digite um número: '))
+
+if numero < 0:
+    print('impossível!')
+    print('Não precisa se alistar.')
+
+elif numero < 18:
+    print('Não precisa se alistar.')
+
+elif 18 < numero < 65:
+    print('Não esqueça de votar na próxima eleição')
+
+elif numero > 65:
+    print('Vá descansar.')
+else: 
+    print('eita!')
+
+
+
+
+
+
+
+        
